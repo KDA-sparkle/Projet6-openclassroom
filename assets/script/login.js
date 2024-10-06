62b5a1426b58305a3b37f97502c1ba2b9e12d57f
KDA-sparkle <125574085+KDA-sparkle@users.noreply.github.com>
2024-10-06 14:26:27 +0200

    Ajout des champs email et mot de passe dans login.js

--- assets/script/login.js
+++ assets/script/login.js
@@ -10,3 +10,7 @@
 const alredyLoggedError = document.querySelector(".alredyLogged__error"); // * Erreur : utilisateur déjà connecté
 const loginEmailError = document.querySelector(".loginEmail__error"); // * Erreur : email incorrect
 const loginMdpError = document.querySelector(".loginMdp__error"); // * Erreur : mot de passe incorrect
+
+// * Sélection des champs email et mot de passe
+const email = document.getElementById("email"); // * Champ email
+const password = document.getElementById("password"); // * Champ mot de passe
