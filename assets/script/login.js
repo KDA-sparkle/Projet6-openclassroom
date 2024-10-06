5541d21c189fb57633cff8895db4cf31a4baefc7
KDA-sparkle <125574085+KDA-sparkle@users.noreply.github.com>
2024-10-06 14:22:41 +0200

    Ajout de la légende dans login.js

---
+++ assets/script/login.js
@@ -0,0 +1,6 @@
+//! ================================ LÉGENDE ================================
+//! * Important : Informations importantes
+//! ! Critique : Erreurs ou points à corriger immédiatement
+//! ? Question : À clarifier ou vérifier
+//! // TODO : À faire plus tard
+//! ========================================================================
