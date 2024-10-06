e96d1dcb72702520ce5a5475f013b5a4d08b3741
KDA-sparkle <125574085+KDA-sparkle@users.noreply.github.com>
2024-10-06 14:23:22 +0200

    Ajout des variables globales dans login.js

--- assets/script/login.js
+++ assets/script/login.js
@@ -4,3 +4,9 @@
 //! ? Question : À clarifier ou vérifier
 //! // TODO : À faire plus tard
 //! ========================================================================
+//! -------------------------------------------------------------- VARIABLES GLOBALES ----------------------------------------------------------------------
+
+// * Sélection des éléments d'affichage des erreurs
+const alredyLoggedError = document.querySelector(".alredyLogged__error"); // * Erreur : utilisateur déjà connecté
+const loginEmailError = document.querySelector(".loginEmail__error"); // * Erreur : email incorrect
+const loginMdpError = document.querySelector(".loginMdp__error"); // * Erreur : mot de passe incorrect
