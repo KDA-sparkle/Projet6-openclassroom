86dc354ffff64ffd17106b3499c9ba12513335cf
KDA-sparkle <125574085+KDA-sparkle@users.noreply.github.com>
2024-10-06 14:34:36 +0200

    Ajout de la légende dans modal.js

---
+++ assets/script/modal.js
@@ -0,0 +1,6 @@
+//! ================================ LÉGENDE ================================
+//! * Important : Informations essentielles ou instructions importantes.
+//! ! Critique : Points à corriger immédiatement, erreurs ou avertissements graves.
+//! ? Question : À clarifier, suggestions ou points à discuter.
+//! // TODO : Tâches à réaliser plus tard ou améliorations à faire.
+//! ========================================================================
