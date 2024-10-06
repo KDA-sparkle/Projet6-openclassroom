7fab89d6277c4b0fc6ed37ec6d36147ef9517513
KDA-sparkle <125574085+KDA-sparkle@users.noreply.github.com>
2024-10-06 14:34:55 +0200

    Ajout des variables pour la génération des projets

--- assets/script/modal.js
+++ assets/script/modal.js
@@ -4,3 +4,23 @@
 //! ? Question : À clarifier, suggestions ou points à discuter.
 //! // TODO : Tâches à réaliser plus tard ou améliorations à faire.
 //! ========================================================================
+
+/////////////////////////////////////////////////////
+// Gestion de la home page /////////////////////////
+/////////////////////////////////////////////////////
+// * >>> GÉNÉRATION DES PROJETS
+
+// * Sélectionne les boutons de filtre sur la page d'accueil pour filtrer les projets
+const btnAll = document.querySelector(".filter__btn-id-null"); // * Bouton qui affichera tous les projets
+const btnId1 = document.querySelector(".filter__btn-id-1"); // * Bouton pour filtrer par catégorie 1 (Objets)
+const btnId2 = document.querySelector(".filter__btn-id-2"); // * Bouton pour filtrer par catégorie 2 (Appartements)
+const btnId3 = document.querySelector(".filter__btn-id-3"); // * Bouton pour filtrer par catégorie 3 (Hôtels & restaurants)
+
+// * Sélection de la section HTML où seront affichés les projets
+const sectionProjets = document.querySelector(".gallery"); // * Section où les projets vont être insérés dynamiquement
+
+let data = null; // * Variable pour stocker les données des projets récupérées
+let id; // * Stocke l'ID du filtre de projet utilisé
+
+// * Appel initial pour afficher tous les projets
+generationProjets(data, null); // * Appelle generationProjets avec null pour afficher tous les projets
